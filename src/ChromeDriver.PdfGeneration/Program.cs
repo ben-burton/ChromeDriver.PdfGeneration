107de2a9e2c51b3b7dad8aebd9232fe658471c4b
Ben Burton <ben.burton@commify.com>
2020-02-21 12:02:37 +0000

    Generate PDF using Chrome command

---
+++ src/ChromeDriver.PdfGeneration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenQA.Selenium.Chrome;
+
+namespace ChromeDriver.PdfGeneration
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var chromeOptions = new ChromeOptions();
+
+            chromeOptions.AddArgument("--headless");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddExcludedArgument("enable-automation");
+            chromeOptions.AddAdditionalCapability("useAutomationExtension", false);
+
+            var driver = new OpenQA.Selenium.Chrome.ChromeDriver(chromeOptions);
+
+            driver.Navigate().GoToUrl($"file:///{Environment.CurrentDirectory}\\HTMLPage1.html");
+
+            var result = (Dictionary<string, object>)driver.ExecuteChromeCommandWithResult("Page.printToPDF", new Dictionary<string, object>
+            {
+                { "displayHeaderFooter", false },
+                { "transferMode", "ReturnAsBase64" }
+            });
+
+            File.WriteAllBytes($"{Environment.CurrentDirectory}\\example_letter.pdf", Convert.FromBase64String((string)result["data"]));
+
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
